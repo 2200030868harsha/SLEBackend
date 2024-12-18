58524ce9673a97cef16b7e5bb643c8f0a5a46dde
2200030868harsha <152666537+2200030868harsha@users.noreply.github.com>
2024-12-18 13:17:56 +0000

    Updated Code for Deployment

--- Sustainable-Living-education (1)/Sustainable-Living-education/src/main/java/com/university/Sustainable_Living_education/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.university.Sustainable_Living_education.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not found the user with id "+ id);
-    }
-}
--- Sustainable-Living-education (1)/Sustainable-Living-education/src/main/java/com/university/Sustainable_Living_education/repository/LessonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.university.Sustainable_Living_education.repository;
-
-import com.university.Sustainable_Living_education.model.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonRepository extends JpaRepository<Lesson, Long> {
-	
-}
